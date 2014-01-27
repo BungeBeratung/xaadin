91470e41469d06005c8b35d63635185150459186
Hendrik Jürgens <github@dncornholio.de>
2014-01-27 12:05:33 +0100

    - Changed SplitPanelElement.spacerPosition attribute to spacerPosition - Add deprecated logger warning to factory

--- src/main/java/com/xaadin/elementfactory/SplitPanelElementFactory.java
+++ src/main/java/com/xaadin/elementfactory/SplitPanelElementFactory.java
@@ -6,16 +6,20 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.VerticalSplitPanel;
 import com.xaadin.VisualTreeNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SplitPanelElementFactory extends AbstractDefaultElementFactory {
+
+    final static Logger logger = LoggerFactory.getLogger(SplitPanelElementFactory.class);
+
     @Override
 	public void addComponentToParent(VisualTreeNode parent, VisualTreeNode child) throws ElementFactoryException {
 		AbstractSplitPanel panel = parent.getComponent();
 
-		int childsAdded = Integer.parseInt(parent.getAdditionalParameter("firstChildAdded", "0"));
-
-		switch (childsAdded) {
-			case 0:
+        int componentCount = panel.getComponentCount();
+        switch (componentCount) {
+            case 0:
 				panel.setFirstComponent((Component) child.getComponent());
 				break;
 			case 1:
@@ -25,13 +29,20 @@ public class SplitPanelElementFactory extends AbstractDefaultElementFactory {
 				throw new ElementFactoryException("Splitpanels can only have two child controls");
 		}
 
-		float spacerPosition = getFloatFromVisualTreeNode("AbstractSplitPanel.splitPosition", parent);
-		if (Math.abs(spacerPosition) > 0.01f) {
+        float spacerPosition;
+        if (!parent.getAdditionalParameter("AbstractSplitPanel.splitPosition", "").isEmpty()) {
+            spacerPosition = getFloatFromVisualTreeNode("AbstractSplitPanel.splitPosition", parent);
+            logger.warn("Deprecated attribute 'AbstractSplitPanel.splitPosition' used for "
+                    + parent.getComponent().getClass().getName()
+                    + ". Attribute will be removed in further versions of xaadin. Please only use the attribute 'splitPosition'.");
+        } else {
+            spacerPosition = getFloatFromVisualTreeNode("splitPosition", parent);
+
+        }
+
+        if (Math.abs(spacerPosition) > 0.01f) {
 			panel.setSplitPosition(spacerPosition * 100.f, Sizeable.Unit.PERCENTAGE);
 		}
-
-		childsAdded++;
-		parent.setAdditionalParameter("firstChildAdded", Integer.toString(childsAdded));
 	}
 
 	@Override
