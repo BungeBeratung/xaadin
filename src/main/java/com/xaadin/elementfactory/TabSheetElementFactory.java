28985e9aae9a62ebd4f658a1c96dd064e212f0b1
Hendrik Jürgens <github@dncornholio.de>
2014-10-28 13:28:38 +0100

    Some minor xsd bugfixes Add Image control

--- src/main/java/com/xaadin/elementfactory/TabSheetElementFactory.java
+++ src/main/java/com/xaadin/elementfactory/TabSheetElementFactory.java
@@ -9,7 +9,7 @@ public class TabSheetElementFactory extends AbstractDefaultLayoutElementFactory 
 	@Override
 	public void addComponentToParent(VisualTreeNode parent, VisualTreeNode child) throws ElementFactoryException {
 		TabSheet sheet = parent.getComponent();
-		String caption = child.getAdditionalParameter("TabSheet.caption", "");
+		String caption = child.getAdditionalParameter("tabSheet.caption", "");
 		sheet.addTab((Component) child.getComponent(), caption);
 	}
 
